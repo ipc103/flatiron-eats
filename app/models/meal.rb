68c59517c604e129f9f53b3ed525043139a9e2c8
ipc103 <iancandysemail@gmail.com>
2015-04-08 10:33:13 -0400

    add meal create page

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,7 +2,7 @@ class Meal < ActiveRecord::Base
   belongs_to :host, class_name: "User"
   has_many :user_meals
   has_many :users, through: :user_meals
-  has_many :restaurants
+  belongs_to :restaurant
   has_many :reviews
   validates :meal_type, :date, :name,  presence: true
 end
