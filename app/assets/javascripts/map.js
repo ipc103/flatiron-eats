f22c11e902052c9bc4f79b2ec0018b521ea164f2
ipc103 <iancandysemail@gmail.com>
2015-04-08 12:00:53 -0400

    remove vendor js files, add document ready function

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,10 +1,10 @@
-$(function(){
+$(".meals.index").ready (function(){
   insertMap(40.705329, -74.013970)
 })
 
 
 function insertMap(lat, lng){
-   var myLatlng = new google.maps.LatLng(40.705329, -74.013970);
+  var myLatlng = new google.maps.LatLng(40.705329, -74.013970);
   var mapCanvas = document.getElementById('map-canvas');
   var mapOptions = {
     center: new google.maps.LatLng(lat, lng),
