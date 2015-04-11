05b9fdfee59a9e70ac913298f969f0a2735299ab
ipc103 <iancandysemail@gmail.com>
2015-04-11 12:59:26 -0400

    add restaurant pins

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,17 +1,36 @@
 $(".meals.index").ready (function(){
   $.getJSON('/meals', function(data){
-    insertMap(40.705329, -74.013970, data);
+    insertMealsMap(40.705329, -74.013970, data);
   }) 
 })
 
 $(".restaurants.index").ready (function(){
   $.getJSON('/restaurants', function(data){
-    insertMap(40.705329, -74.013970, data);
+    insertRestaurantsMap(40.705329, -74.013970, data);
   }) 
 })
 
 
-function insertMap(lat, lng, data){
+function insertRestaurantsMap(lat, lng, data){
+  var myLatlng = new google.maps.LatLng(40.705329, -74.013970);
+  var mapCanvas = document.getElementById('map-canvas');
+  var mapOptions = {
+    center: new google.maps.LatLng(lat, lng),
+    zoom: 16,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var img = "assets/logo.png"
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      icon: img,
+      title: 'Hello World!'
+  });
+  insertRestaurantPins(map, data);
+}
+
+function insertMealsMap(lat, lng, data){
   var myLatlng = new google.maps.LatLng(40.705329, -74.013970);
   var mapCanvas = document.getElementById('map-canvas');
   var mapOptions = {
@@ -29,4 +48,3 @@ function insertMap(lat, lng, data){
   });
   insertMealsPins(map, data);
 }
-
