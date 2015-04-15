c5eb4a9b5c736301227c4d8bf140612f3c9315ec
ipc103 <iancandysemail@gmail.com>
2015-04-15 16:53:35 -0400

    finish date calendar

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -1,3 +1,10 @@
+
 $(function() {
-    $( "#datepicker" ).datetimepicker();
+  var dateToday = new Date();
+  $( "#datepicker" ).datetimepicker({
+    // format: 'MM-DD-YYYY HH:MM',
+    minDate: dateToday,
+    sideBySide: true
+    // dateFormat: "yyyy-mm-dd"
+  });
 });
