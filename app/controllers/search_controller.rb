6195148f555c841c9fcb2ed83d706a77e265187e
deniznida <denizunat@hotmail.com>
2015-04-16 15:17:52 -0400

    style meals page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ class SearchController < ApplicationController
 
 	def food_categories
 		@restaurants = Category.find(params[:categories]).collect {|c| c.restaurants}.flatten.uniq
+
     respond_to do |f|
       f.js{
         render "restaurants/results"
