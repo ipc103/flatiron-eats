7c278fea56c55c133fca07655eb87f26d605feb6
ipc103 <iancandysemail@gmail.com>
2015-04-16 13:39:21 -0400

    refactor insertMeals method

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,17 +1,17 @@
 $(".meals.index").ready (function(){
   $.getJSON('/meals', function(data){
-    insertMealsMap(40.705329, -74.013970, data);
+    insertMap(40.705329, -74.013970, data, insertMealsPins)
   });
 });
 
 $(".restaurants.index").ready (function(){
   $.getJSON('/restaurants', function(data){
-    insertRestaurantsMap(40.705329, -74.013970, data);
+    insertMap(40.705329, -74.013970, data, insertRestaurantPins);
   }); 
 });
 
 
-function insertRestaurantsMap(lat, lng, data){
+function insertMap(lat, lng, data, funk){
   var myLatlng = new google.maps.LatLng(40.705329, -74.013970);
   var mapCanvas = document.getElementById('map-canvas');
   var mapOptions = {
@@ -27,24 +27,24 @@ function insertRestaurantsMap(lat, lng, data){
       icon: img,
       title: 'Hello World!'
   });
-  insertRestaurantPins(map, data);
+  funk(map, data);
 }
 
-function insertMealsMap(lat, lng, data){
-  var myLatlng = new google.maps.LatLng(40.705329, -74.013970);
-  var mapCanvas = document.getElementById('map-canvas');
-  var mapOptions = {
-    center: new google.maps.LatLng(lat, lng),
-    zoom: 16,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-  var img = "assets/logo.png";
-  var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      icon: img,
-      title: 'Hello World!'
-  });
-  insertMealsPins(map, data);
-}
+// function insertMealsMap(lat, lng, data){
+//   var myLatlng = new google.maps.LatLng(40.705329, -74.013970);
+//   var mapCanvas = document.getElementById('map-canvas');
+//   var mapOptions = {
+//     center: new google.maps.LatLng(lat, lng),
+//     zoom: 16,
+//     mapTypeId: google.maps.MapTypeId.ROADMAP
+//   };
+//   var map = new google.maps.Map(mapCanvas, mapOptions);
+//   var img = "assets/logo.png";
+//   var marker = new google.maps.Marker({
+//       position: myLatlng,
+//       map: map,
+//       icon: img,
+//       title: 'Hello World!'
+//   });
+//   funk(map, data);
+// }
