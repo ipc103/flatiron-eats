3d0b6c856a940a14f045e58c8468de16dded5ac0
ipc103 <iancandysemail@gmail.com>
2015-04-17 11:09:59 -0400

    make search button change text

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,5 +1,13 @@
 $(function() {
   $( "#category-search" ).on("click", function(){
-    $( "div#search-boxes" ).slideToggle('slow')
+    $button = $(this)
+    $( "div#search-boxes" ).slideToggle('slow', function(){
+      if ( $button.html() === 'Display Search Options'){
+        $button.html('Hide Search Options');
+      } else {
+        $button.html('Display Search Options')
+      }
+        
+    })
   })
 });
